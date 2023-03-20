007ecc4a938a120e98428ddd698992d62bb91d35
Surendhar <studyspace.ie@gmail.com>
2023-03-20 22:41:23 +0530

    built a skeleton app

---
+++ pages/code.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const code = () => {
+  return (
+    <>
+        <header>
+            <h1>HTML</h1>
+            <h1>CSS</h1>
+            <h1>React</h1>
+        </header>
+    </>
+  )
+}
+
+export default code
---
+++ pages/importFile.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const importFile = () => {
+  return (
+    <> 
+    <header>
+        <h1>Select Frames:</h1>
+    </header>
+    <main>
+        {/* list of frames */}
+        <button>Submit</button>
+        <button>Cancel</button>
+    </main>
+    </>
+   
+  )
+}
+
+export default importFile
