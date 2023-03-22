9cb0f1ec8b65b77a5a9b39129a9dde89c23c8e59
Surendhar <studyspace.ie@gmail.com>
2023-03-22 16:14:22 +0530

    fetched file Id

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import {useSearchParams} from 'next/navigation'
+import {useRouter} from 'next/router'
 
-const code = () => {
+
+const figmaId="fsOaVXAaLiSB6pxs91vohx";
+const figmaApi="figd_P2QEjM92xOVpiDqQDG4FpCiIPW3v7KFIb8G3P3Rd"
+
+// id, name, type, scrollBehavior, children, backgroundColor, prototypeStartNodeID, flowStartingPoints, prototypeDevice}
+
+const Code = () => {
+
+    const router=useRouter()
+    const {
+        query:{value}
+    }=router
+
+    const props={value}
+
+
   return (
     <>
         <header>
@@ -8,8 +25,27 @@ const code = () => {
             <h1>CSS</h1>
             <h1>React</h1>
         </header>
+
+        <div>
+            {props.value}
+        </div>
     </>
   )
 }
 
-export default code
+export const getServerSideProps=async()=>{
+    const result=await fetch(`https://api.figma.com/v1/files/${figmaId}`,{
+        method: "GET",
+        headers:{
+            "X-Figma-Token":figmaApi
+        }
+    })
+    const figmaStructure=await result.json()
+    const document=figmaStructure.document.children
+    
+    return {
+        props:{document}
+    }
+}
+
+export default Code
