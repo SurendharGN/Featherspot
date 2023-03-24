d3473a3832b941bf7c22cb50c9f001320ca4bd26
Surendhar <studyspace.ie@gmail.com>
2023-03-24 11:53:27 +0530

    removed styles and fixed a major bug.

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,5 +1,7 @@
+// LEARN ABOUT USEROUTER HOOK
+ {/* <p>{value.split("/")[4]</p>  */}
 import React from 'react'
-
+import { useState } from 'react';
 import {useRouter} from 'next/router'
 import Link from 'next/link';
 import axios from 'axios';
@@ -11,13 +13,6 @@ const figmaToken='figd_7RQOFQ_3nafYmOanvT1ICmMO5jkyd6hKmTIooqjh'
 
 const Code = ({document}: any) => {
 
-    const router=useRouter()
-    const {
-        query:{value}
-    }=router
-
-    const props={value}
-    const userToken=
     async()=>{
     const response=await fetch("https://www.figma.com/api/oauth/token?client_id=jqf4nL30juMiPSpgOFQJUT&client_secret=E0mGbu9qZv8h8jHFmqrbeIOg1VktBx&redirect_uri=http://localhost:3001/Code&code=rE0Pexb5SOhIbwPqhJMrlZo6w&grant_type=authorization_code",{
             method:"POST",
@@ -30,7 +25,11 @@ const Code = ({document}: any) => {
         return {data}
         
     }
-    
+
+    const [clicked,setClicked]=useState(false)
+    const getURL=()=>{
+        setClicked(!clicked)
+    }
 
   return (
     <>
@@ -41,20 +40,27 @@ const Code = ({document}: any) => {
         </header>
 
         <div>
-            <p>{props.value}</p>
+            {/* <p>{urlId}</p> */}
+            {/* <p>{currentURL.asPath}</p> */}
             {document[0].name}
             
         </div>
 
-        <a href='https://www.figma.com/oauth?client_id=jqf4nL30juMiPSpgOFQJUT&redirect_uri=http://localhost:3001/Code&scope=file_read&state=test&response_type=code' >Authenticate</a>
-        <button onClick={userToken}>Token</button>
+         <header>
+            
+      </header>
+      <main>
+
+        <button onClick={getURL}>Token</button>
+        
+      </main>
+
+        <Link href={`https://www.figma.com/oauth?client_id=jqf4nL30juMiPSpgOFQJUT&redirect_uri=http://localhost:3001/Code&scope=file_read&state=test&response_type=code`}  >Authenticate</Link>
+        {clicked && <input placeholder='URL'/>}
+        
     </>
   )
 }
-
-// axios.post("").then(response=>console.log(response))
-
-
 
 export const getServerSideProps=async()=>{
     const result=await fetch(`https://api.figma.com/v1/files/fsOaVXAaLiSB6pxs91vohx`,{
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from "next/link"
 import { useState } from 'react'
-import { Router,useRouter } from 'next/router';
-
-
 import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
-    const router=useRouter()
-    const urlRouter=useRouter()
+   
 
-    const [value,setValue]=useState('')
+   
     
-    function sendUrl(){
-        urlRouter.push({
-            pathname:'/Code',
-            query:{value}
-        })
-    }
+
   return (
     <>
-    
-      <header>
-       <h1>Featherspot</h1>
-      </header>
-      <main>
-        <h2>Import URL:</h2>
-        <input onChange={(event)=>setValue(event.target.value)} placeholder="URL"></input>
-        <button onClick={sendUrl}>Import</button>
+        <Link href='/Code'>Take me to the page</Link>
         
-        
-        
-      </main>
-     
-      
     </>
   )
 }
